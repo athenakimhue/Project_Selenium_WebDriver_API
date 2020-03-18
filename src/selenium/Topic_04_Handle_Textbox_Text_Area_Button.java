baad0d5889c7cfb8034117ac20d7cd4af2285991
athenkimhue <athenkimhue@github.com>
2020-03-19 00:08:59 +0700

    18/03/2020 - 24h Topic 04

--- src/selenium/Topic_04_Handle_Textbox_Text_Area_Button.java
+++ src/selenium/Topic_04_Handle_Textbox_Text_Area_Button.java
@@ -16,7 +16,7 @@ import org.testng.annotations.Test;
 
 public class Topic_04_Handle_Textbox_Text_Area_Button {
 	WebDriver driver;
-	String emailAddress, name, gender, dob, address, city, state, pincode, telephone, emailid, password;  
+	String emailAddress, name, gender, dob1,dob2, address, city, state, pincode, telephone, emailid, password;  
 	By nameTextboxBy	= By.xpath("//input[@name='name']");
 	By genderBy			= By.xpath("//input[@value='f']");
 	By dobBy			= By.xpath("//input[@id='dob']");
@@ -31,10 +31,17 @@ public class Topic_04_Handle_Textbox_Text_Area_Button {
 
 	@BeforeClass
 	public void beforeClass() {
-		driver = new FirefoxDriver();
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("headless");
+//		options.addArguments("window-size=1366x768");
+//		driver = new ChromeDriver(options);
+		driver = new ChromeDriver();
 		emailAddress= "kimhue" + numberRandom() + "@gmail.com";
 	}
 
+	/**
+	 * @throws InterruptedException
+	 */
 	@Test
 	public void TC_01() throws InterruptedException {
 
@@ -66,17 +73,18 @@ public class Topic_04_Handle_Textbox_Text_Area_Button {
 		
 		//Resgister New Customer
 		name = "John";
-		dob = "2010-01-01";
+		dob1 = "2010-01-01";
+		dob2 = "01/01/2010";
 		address = "Hung Yen";
 		city = "Ha Noi";
 		state = "Single";
 		pincode = "123456";
 		telephone = "12345678";
-		password = "123456";  
+		password = "12345678";  
 		
 		find(nameTextboxBy).sendKeys(name);
 		find(genderBy).click();
-		find(dobBy).sendKeys(dob);
+		find(dobBy).sendKeys(dob2);
 		find(addressBy).sendKeys(address);
 		find(cityBy).sendKeys(city);
 		find(stateBy).sendKeys(state);
@@ -89,7 +97,7 @@ public class Topic_04_Handle_Textbox_Text_Area_Button {
 		
 		//Verify New Customer
 		Assert.assertEquals(find(By.xpath("//td[text()='Customer Name']/following-sibling::td")).getText(),name);
-		Assert.assertEquals(find(By.xpath("//td[contains(text(),'Birthdate')]/following-sibling::td")).getText(), dob);
+		Assert.assertEquals(find(By.xpath("//td[contains(text(),'Birthdate')]/following-sibling::td")).getText(), dob1);
 		Assert.assertEquals(find(By.xpath("//td[text()='Address']/following-sibling::td")).getText(), address);
 		Assert.assertEquals(find(By.xpath("//td[contains(text(),'City')]/following-sibling::td")).getText(), city);
 		Assert.assertEquals(find(By.xpath("//td[contains(text(),'State')]/following-sibling::td")).getText(), state);
@@ -97,11 +105,16 @@ public class Topic_04_Handle_Textbox_Text_Area_Button {
 		Assert.assertEquals(find(By.xpath("//td[contains(text(),'Mobile No.')]/following-sibling::td")).getText(), telephone);
 		Assert.assertEquals(find(By.xpath("//td[contains(text(),'Email')]/following-sibling::td")).getText(), emailAddress);
 
-		
-	
 		String customerID=find(By.xpath("//td[contains(text(),'Customer ID')]/following-sibling::td")).getText();
 		System.out.println("CustomerID la :"+ customerID);
+		
+		//Edit Customer
+		find(By.xpath("//a[contains(text(),'Edit Customer')]")).click();
+		find(By.xpath("//input[@name='cusid']")).sendKeys(customerID);
+		find(By.xpath("//input[@name='AccSubmit']")).click();
+		
 	}
+
 
 
 	public int numberRandom() {
